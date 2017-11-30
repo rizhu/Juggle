b83ad616c10790fa296c280de48ca2778d07c3eb
FiishCo <fiish.costudios@gmail.com>
2017-11-29 21:34:26 -0800

    Initial commit

---
+++ core/src/com/gmail/studios/co/fiish/juggle/JuggleGame.java
@@ -0,0 +1,8 @@
+package com.gmail.studios.co.fiish.juggle;
+
+import com.badlogic.gdx.Game;
+
+public class JuggleGame extends Game {
+	@Override
+	public void create() { }
+}
---
+++ desktop/src/com/gmail/studios/co/fiish/juggle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gmail.studios.co.fiish.juggle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gmail.studios.co.fiish.juggle.JuggleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JuggleGame(), config);
+	}
+}
